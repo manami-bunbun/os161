c92d7cb93a58bd8db19af9d2e822b991e639313d
Manami Nakagawa <30760730+manami-bunbun@users.noreply.github.com>
2022-03-21 14:36:23 +0000

    add cv_signal things

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -239,7 +239,7 @@ cv_destroy(struct cv *cv)
 	KASSERT(cv != NULL);
 
 	// add stuff here as needed
-
+	wchan_destroy(cv->cv_wchan);
 	kfree(cv->cv_name);
 	kfree(cv);
 }
@@ -248,6 +248,11 @@ void
 cv_wait(struct cv *cv, struct lock *lock)
 {
 	// Write this
+	KASSERT( cv != NULL );
+	wchan_lock(cv->cv_wchan);
+	lock_release(lock);
+	wchan_sleep(cv->cv_wchan);
+	lock_acquire(lock);
 	(void)cv;    // suppress warning until code gets written
 	(void)lock;  // suppress warning until code gets written
 }
@@ -256,6 +261,9 @@ void
 cv_signal(struct cv *cv, struct lock *lock)
 {
 	// Write this
+	KASSERT( lock_do_i_hold(lock) );
+	wchan_wakeone(cv->cv_wchan);
+
 	(void)cv;    // suppress warning until code gets written
 	(void)lock;  // suppress warning until code gets written
 }
@@ -264,6 +272,8 @@ void
 cv_broadcast(struct cv *cv, struct lock *lock)
 {
 	// Write this
+	KASSERT( lock_do_i_hold(lock) );
+	wchan_wakeall(cv->cv_wchan);
 	(void)cv;    // suppress warning until code gets written
 	(void)lock;  // suppress warning until code gets written
 }
